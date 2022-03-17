27d8698d341bf060f2d8795061165f59c7b60cc8
vali-pavel <valeriu.pavel92@gmail.com>
2022-03-17 11:26:26 +0200

    create the signup page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
 import useToken from './useToken';
 import './App.css';
 
 function App() {
     const { token, setToken } = useToken();
 
-    if (!token) {
-        return <Login setToken={setToken} />
-    }
-
     return (
         <div className="wrapper">
             <Router>
                 <Routes>
-                    <Route path='/login' element={<Login />} />
+                    <Route path='/login' element={<Login setToken={setToken} />} />
+                    <Route path='/signup' element={<Signup setToken={setToken} />} />
+                    <Route path='/admin/signup' element={<Signup setToken={setToken} />} />
                 </Routes>
             </Router>
         </div>
