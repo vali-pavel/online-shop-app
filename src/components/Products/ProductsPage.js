fbb78cbee43ccfe40158a926e7c59be50dd4297f
vali-pavel <valeriu.pavel92@gmail.com>
2022-03-18 12:20:15 +0200

    update the ProductsPage component
    - render the products
    - render a Pagination component
    - fetch products based on pagination
    - show the 1-click checkout button if user is customer

--- src/components/Products/ProductsPage.js
+++ src/components/Products/ProductsPage.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Col, Row, Container } from 'react-bootstrap';
 
 import { userRoles } from '../enums';
+import Checkout from '../Checkout/Checkout';
+import PaginationComponent from './Pagination';
 
 const tokenString = localStorage.getItem('token');
 
 export default function ProductsPage({ userId, userRole }) {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState();
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [error, setError] = useState()
 
     useEffect(() => {
-        async function getProducts(userId) {
-            return fetch(`/user_products?user_id=${ userId }`, {
+        setLoading(true);
+        async function getProducts() {
+            const requestUrl = userRole === userRoles.Merchant ?
+                `/api/products?user_id=${ userId }&page_number=${ currentPage }` :
+                `/api/products?page_number=${ currentPage }`;
+
+            return fetch(requestUrl, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -21,21 +32,42 @@ export default function ProductsPage({ userId, userRole }) {
                 .then(async response => {
                     const jsonResponse = await response.json()
                     if (!response.ok) {
-                        return { error: jsonResponse }
+                        setError({ error: jsonResponse.error.detail })
                     } else {
-                        return jsonResponse
+                        setProducts(jsonResponse)
                     }
 
                 })
         }
-        const productsResponse = getProducts(userId);
-        console.log("Products ", productsResponse)
-    }, [])
+        getProducts();
+        setLoading(false)
+    }, [currentPage]);
+
+
+
+    const totalPages = products && Math.ceil(products.total / products.size);
 
     return (
-        userRole === userRoles.Merchant ? <div>
-
-        </div> : "You are not authorized to view this page"
+        <div>
+            {error}
+            {products !== undefined ? <Container>
+                {products.items.map((product, index) => <div key={product.id}>
+                    {index === 0 && <Row>
+                        <Col>Name</Col>
+                        <Col>Price</Col>
+                        {userRole === userRoles.Customer && <Col xs={2}></Col>}
+                    </Row>}
+                    <Row key={product.id} className="d-flex justify-content-between flex-direction-column">
+                        <Col className="product-name">{product.name}</Col>
+                        <Col className="product-price">{product.price}</Col>
+                        {userRole === userRoles.Customer && <Col xs={2}>
+                            <Checkout userId={userId} productId={product.id} />
+                        </Col>}
+                    </Row>
+                </div>)}
+                <PaginationComponent totalPages={totalPages} setCurrentPage={setCurrentPage}></PaginationComponent>
+            </Container> : "No Products"}
+        </div>
     )
 }
 
